bea6702531de7af6c02a6d70156047b060049fb1
vinieustaquio29 <viniciuscontabilidade5@gmail.com>
2024-02-05 09:45:39 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, onClick }: IButtonProps) => {
+  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+};
+
+export default Button;
